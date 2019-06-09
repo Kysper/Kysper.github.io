bd8dbab621cac67d92eb90ac6762087e563eeedf
Kysper <kysper50@gmail.com>
2019-06-09 10:09:48 -0500

    Revamped portfolio done except for some CSS transistions

--- assets/javascript/functionality.js
+++ assets/javascript/functionality.js
@@ -15,7 +15,7 @@ var home = new Content(dataHome.id, dataHome.title, dataHome.description, dataLi
 var bio = new Content(dataBio.id, dataBio.title, dataBio.description, dataLinks);
 var portfolio = new Content(dataPortFolio.id, dataPortFolio.title, dataPortFolio.description, dataLinks);
 var contact = new Content(dataContact.id, dataContact.title, dataContact.description, dataLinks);
-var el = [home, bio, portfolio, contact];
+var el = [home, bio, contact, portfolio];
 
 
 document.querySelector('*').addEventListener('click', function (event) {
@@ -34,40 +34,83 @@ document.querySelector('*').addEventListener('click', function (event) {
         case 'portfolio-button':
             document.getElementById('home', 'bio', 'contact').classList.remove('active');
             document.getElementById('portfolio').classList.add('active');
-            setTopPage(el[2]);
+            setTopPage(el[3]);
             break;
         case 'contact-button':
             document.getElementById('bio', 'portfolio', 'home').classList.remove('active');
             document.getElementById('contact').classList.add('active');
-            setTopPage(el[3]);
+            setTopPage(el[2]);
             break;
+        case 'logo':
+            document.getElementById('top-page').innerHTML = 'Recently Clicked:';
     }
 });
 
 //Style layout of the page
 
 
-function setTopPage(content){
+
+function setTopPage(content) {
     var active = document.getElementsByClassName('active');
-    if(active){
-    document.getElementById('top-page').innerHTML = `<h3>Recently Clicked:</h3><br><div id='${content.id}'>
+    if (active && content !== portfolio) {
+        document.getElementById('top-page').innerHTML = `<h3>Recently Clicked:</h3><br><div id='${content.id}'>
     <h2>${content.title}<h2><br>
     <p>${content.desc}</p><br>
     <a href='${content.links.linkURL[0]}'>${content.links.linkName[0]}<br></a> | <a href='${content.links.linkURL[1]}'>${content.links.linkName[1]}<br></a>`
-    }else{
-        document.getElementById('top-page').innerHTML = 'Top Page';
+    } else {
+        console.log('====Error====\nDOM did not update!')
+    } if (active && content === portfolio) {
+        document.getElementById('top-page').innerHTML += html1
+        document.getElementById('top-page').innerHTML += html2;
+        document.getElementById('top-page').innerHTML += html3;
     }
 }
 
-function populatePage(){
-    el.forEach(contents =>{
-        document.getElementById(contents.id).innerHTML = `<div id='${contents.id}'>
+function populatePage() {
+    el.forEach(contents => {
+        if (contents !== portfolio) {
+            document.getElementById(contents.id).innerHTML = `<div id='${contents.id}'>
         <h2>${contents.title}<h2><br>
         <p>${contents.desc}</p><br>
         <a href='${contents.links.linkURL[0]}'>${contents.links.linkName[0]}</a> :: <a href='${contents.links.linkURL[1]}'>${contents.links.linkName[1]}<br></a>`
+        } else {
+            createHTML();
+            populatePortfolio();
+        }
     })
 }
 
-populatePage();
+function populatePortfolio() {
+    document.getElementById('portfolio').innerHTML += html1
+    document.getElementById('portfolio').innerHTML += html2;
+    document.getElementById('portfolio').innerHTML += html3;
+}
 
+function createHTML() {
+    html1 = `<div id='portfolio-wrapper'>
+        <h2 id='title'>${dataPortFolio.title[0]}<h2><br>
+            <img src='https://via.placeholder.com/100'>
+            <p id='desc'>${dataPortFolio.description[0]}</p>
+            <a id='links' href='${data.links.projectLinks[0]}'>${dataPortFolio.title[0]}</a></div>`;
 
+    html2 = `<div id='portfolio-wrapper'>
+            <h2 id='title'>${dataPortFolio.title[1]}<h2><br>
+                <img src='https://via.placeholder.com/100'>
+                <p id='desc'>${dataPortFolio.description[1]}</p>
+                <a id='links' href='${data.links.projectLinks[1]}'>${dataPortFolio.title[1]}</a></div>`;
+
+    html3 = `<div id='portfolio-wrapper'>
+                <h2 id='title'>${dataPortFolio.title[2]}<h2><br>
+                    <img src='https://via.placeholder.com/100'>
+                    <p id='desc'>${dataPortFolio.description[2]}</p>
+                    <a id='links' href='${data.links.projectLinks[2]}'>${dataPortFolio.title[2]}</a></div>`;
+
+    return html1, html2, html3;
+
+}
+
+function init() {
+    populatePage();
+}
+
+init()
--- assets/javascript/json.js
+++ assets/javascript/json.js
@@ -1,11 +1,10 @@
-
 var data = {
 
     home: {
         id: 'home',
         title: 'Home',
         description: `Hello welcome to my profile. I hope you enjoy your stay!`,
-    },
+        },
     bio: {
         id: 'bio',
         title: 'Bio',
@@ -17,19 +16,26 @@ var data = {
         Everything changed when we welcomed our first child into the world this year.
         After our baby girl's birth, I wanted to teach her to follow her dreams fearlessly and persistently.
         In an attempt to show her this, I quit my job of eight years and enrolled in KU Coding Certificate in pursuit of my passions.`
-    },
+        },
     portfolio: {
         id: 'portfolio',
-        title: 'Portfolio',
-        description: 'Projects Here',
+        title: ["What's for Dinner","Christmas List","Abacus"],
+        description: ["What's for Dinner is a recipe finder that helps you create weekly meal plans. Also if you don't feel like eating at home that day"+
+        "it has google maps built-in to give you various restaurants around the area.",
+        "Christmas List App helps you get the gifts you want and buy gifts for those who is friends of yours by seeing their gift list."+
+        "It uses the Wal-Mart API to find gifts for yourself and or buy them for others from their gift lists.",
+        "Abacus is a teacher assistant in which you can add and remove students, grade and organize assignments, and add and remove classroom's "],  
     },
     contact: {
         id: 'contact',
         title: 'Contact',
         description: 'Contact Me Here',
-    },
+        },
     links: {
-        linkName: ['GitHub', 'LinkedIn'],
-        linkURL: ['www.github.com/Kysper', 'www.linkedin.com/vadams0805']
+        linkName: ['GitHub', 'LinkedIn'
+            ],
+        linkURL: ['www.github.com/Kysper', 'www.linkedin.com/vadams0805'
+            ],
+        projectLinks:["https://kysper.github.io/WhatsforDinner/",'Christmas List','Abacus']
+        }
     }
-}
